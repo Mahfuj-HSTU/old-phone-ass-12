6d2eaec01ad548902c9afdd6a970266744591ec6
Mahfuj-HSTU <rmahfujur16@gmail.com>
2022-11-25 14:49:50 +0600

    footer added

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -9,17 +9,17 @@ const Header = () => {
     </>
 
     return (
-        <div className="navbar h-20 mb-12 bg-base-100">
+        <div className="navbar bg-base-300 mb-7 py-3">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={ 0 } className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <u tabIndex={ 0 } className="menu menu-compact dropdown-content p-2 shadow bg-base-500 rounded-box w-52">
+                    <ul tabIndex={ 0 } className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         { menuItems }
-                    </u>
+                    </ul>
                 </div>
-                <Link to='/' className="btn btn-ghost normal-case text-xl"><img src={ logo } alt="" width='50px' height='50px' /></Link>
+                <Link to='/' className="btn btn-ghost normal-case text-xl"><img src={ logo } alt="" width='50px' height='50px' /> Super Sale</Link>
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
